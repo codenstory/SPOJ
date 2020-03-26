27a48d0a3fab24cd49259f00f4eb1c2cb2640cef
kazi.serikzhan@gmail.com <Dalgrad2020>
2020-03-26 08:13:28 -0300

    GCJ'09 Round 2 Problem D is giving WA

--- gcj11rnd1ApB.cpp
+++ gcj11rnd1ApB.cpp
@@ -1,6 +1,8 @@
+#include <utility>
+
 /**
- * TOPIC: DP, fast simulation
- * status:
+ * TOPIC: DP, fast simulation, partition refinement, bitmasks, STL, sorting, bfs, very neat
+ * status: Accepted
  */
 #include <bits/stdc++.h>
 using size_type= int;
@@ -11,88 +13,113 @@ struct word {
     int weight;
     std::string s;
     int idx;
-    word() {};
-    word( int w, std::string s, int i ): weight(w), s(s), idx(i) {};
 };
 
 bool operator < ( const word &a, const word &b ) {
     return a.weight < b.weight or (a.weight == b.weight and a.idx < b.idx);
 }
 
-std::map<size_type,std::vector<word>> buckets;
 int n,qr,z[N][A];
-std::vector<std::string> D;
+unsigned int mask[N],profile[N],all_locations[N][A];
+std::vector<word> D;
+
+int solve( const std::string &L ) {
+    int i,j,k;
+    std::map<char,int> pos{};
+    for ( i= 0; i < L.size(); ++i )
+        pos[L[i]]= i;
+    std::sort(D.begin(),D.end(),[](const auto &a, const auto &b) {
+        return a.s.size() < b.s.size();
+    });
+    std::queue<std::tuple<int,int,int>> q{};
+    for ( i= 0; i < n; q.push({0,i,j}), i= j )
+        for ( j= i+1; j < n and D[i].s.size() == D[j].s.size(); ++j ) ;
+    for ( i= 0; i < n; ++i )
+        D[i].weight= pos[
+            *std::max_element(D[i].s.begin(),D[i].s.end(),[&pos](auto x,auto y) {
+                return pos[x] < pos[y];
+            })];
+    for ( i= 0; i < n; ++i )
+        for ( j= 0; j <= L.size(); z[i][j++]= -1 ) ;
+    for ( i= 0; i < n; ++i )
+        z[i][0]= 0;
+    for ( i= 0; i < n; ++i )
+        for ( mask[i]= 0, j= 0; j < A; all_locations[i][j++]= 0 ) ;
+    for ( const auto &w: D )
+        for ( j= 0; j < w.s.size(); ++j ) {
+            mask[w.idx] |= (1 << (w.s[j] - 'a'));
+            all_locations[w.idx][w.s[j]-'a']|= (1<<j);
+            profile[w.idx]= (1<<w.s.size())-1;
+        }
+    while ( not q.empty() ) {
+        auto pr= q.front(); q.pop();
+        k= std::get<0>(pr), i= std::get<1>(pr), j= std::get<2>(pr);
+        assert( 0 <= i and j <= n );
+        if ( j-i == 1 ) {
+            // singleton bucket
+            z[D[i].idx][k+1]= z[D[i].idx][k];
+            if ( k+1 < L.size() )
+                q.push({k+1,i,j});
+            continue ;
+        }
+        assert( j-i >= 2 );
+        unsigned int u= 0;
+        for ( auto t= i; t < j; ++t )
+            u|= mask[D[t].idx];
+        unsigned int ll= (1ul<<(L[k]-'a'));
+        for ( auto t= i; t < j; ++t ) {
+            assert( z[D[t].idx][k+1] == -1 );
+            z[D[t].idx][k+1] = z[D[t].idx][k]+((u&ll)?((mask[D[t].idx]&ll)?0:1):0);
+            profile[D[t].idx]&= ~all_locations[D[t].idx][L[k]-'a'];
+            mask[D[t].idx]&= ~ll;
+        }
+        auto &prof= profile;
+        std::sort(D.begin()+i,D.begin()+j,[&prof](const auto &a, const auto &b) {
+            return prof[a.idx] < prof[b.idx];
+        });
+        for ( int t= i, l; t < j; t= l ) {
+            for ( l= t+1; l < j and profile[D[l].idx] == profile[D[t].idx]; ++l ) ;
+            if ( k+1 < L.size() ) {
+                q.push({k+1,t,l});
+            }
+        }
+    }
+    for ( i= 0; i < n; ++i )
+        for ( j= 0; j <= L.size(); ++j )
+            assert( z[i][j] >= 0 );
+    int ans= -1;
+    for ( i= 0; i < n; ++i )
+        if ( ans < z[i][L.size()] )
+            ans= z[k= i][L.size()];
+    return k;
+
+}
 
 int main() {
-    int i,j,k,cs= 0,ts;
+    int i,cs= 0,ts;
     std::string L;
     std::istream &is = std::cin;
     std::ostream &os = std::cout;
     for ( is >> ts; ts--; ) {
         is >> n >> qr, D.resize(n);
-        for ( buckets.clear(), i= 0; i < n; ++i ) {
+        memset(all_locations,0,sizeof all_locations);
+        for ( i= 0; i < n; ++i ) {
             word w;
-            is >> w.s, k= w.s.size();
-            buckets[k].emplace_back(0,w.s,w.idx= i);
-            D[i]= std::move(w.s);
+            is >> w.s, mask[w.idx= i]= 0, profile[w.idx]= (1<<(w.s.size()))-1;
+            for ( int j= 0; j < w.s.size(); ++j ) {
+                mask[w.idx] |= (1 << (w.s[j] - 'a'));
+                all_locations[w.idx][w.s[j]-'a']|= (1<<j);
+            }
+            D[i]= std::move(w);
         }
         std::vector<int> res{};
-        while ( qr-- ) {
-            is >> L;
-            std::map<char,size_type> pos{};
-            for ( i= 0; i < L.size(); ++i )
-                pos[L[i]]= i;
-            for ( auto &it: buckets ) {
-                auto &v= it.second;
-                for ( auto &w: v ) {
-                    w.weight= pos[*std::max_element(w.s.begin(),w.s.end(),[&pos](auto a,auto b) {
-                        return pos[a] < pos[b];
-                    })];
-                }
-                std::sort(v.begin(),v.end());
-            }
-            std::map<size_type,std::pair<
-                    typename std::vector<word>::iterator,
-                    typename std::vector<word>::iterator
-                    >> sliders{};
-            for ( auto &it: buckets )
-                sliders[it.first]= {it.second.begin(),it.second.end()};
-            std::map<int,size_type> position{};
-            for ( const auto &it: buckets ) {
-                const auto &v= it.second;
-                for ( i= 0; i < v.size(); ++i )
-                    position[v[i].idx]= i;
-            }
-
-            assert( L.size() == A-1 );
-            for ( i= 0; i < n; z[i++][0]= 0 ) ;
-            for ( j= 1; j <= L.size(); ++j ) {
-                auto ch= L[j-1];
-                auto weight= pos[ch];
-                for ( i= 0; i < n; ++i ) {
-                    z[i][j]= z[i][j-1];
-                    auto which_bucket= D[i].size();
-                    auto &val= sliders[which_bucket];
-                    if ( val.first == val.second or weight < val.first->weight  )
-                        continue ;
-                    assert( weight == val.first->weight );
-                    for ( ;val.first < val.second and weight == val.first->weight; ++val.first ) ;
-                    assert( position[i] < buckets[which_bucket].size() );
-                    if ( val.second-val.first < buckets[which_bucket].size()-position[i] )
-                        continue ; //we've guessed the word
-                    ++z[i][j];
-                }
-            }
-            int ans= -1;
-            for ( k= 0, i= 0; i < n; ++i )
-                if ( ans < z[i][L.size()] )
-                    ans= z[k= i][L.size()];
-            res.push_back(k);
-            std::cerr << "For order " << L << " the weight is " << ans << std::endl;
-        }
+        for (;qr--; is >> L, res.push_back(solve(L)) ) ;
+        std::sort(D.begin(),D.end(),[](const auto &a,const auto &b) {
+            return a.idx < b.idx;
+        });
         os << "Case #" << ++cs << ":";
         for ( auto x: res )
-            os << " " << D[x];
+            os << " " << D[x].s;
         os << "\n";
     }
     return 0;
