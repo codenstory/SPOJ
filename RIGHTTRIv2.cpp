33bb19fb6536078fca6a509ff36767457bc1aa8a
kazi.serikzhan@gmail.com <Dalgrad2020>
2020-05-31 17:19:58 -0300

    RIGHTTRI, TLE

--- RIGHTTRIv2.cpp
+++ RIGHTTRIv2.cpp
@@ -1,9 +1,10 @@
 /**
  * RIGHTTRI
- * status: W.I.P., WA
+ * status: W.I.P., TLE
  * TOPIC: analytical geometry, rotations, fractions, maths
  */
 #include <bits/stdc++.h>
+#include <boost/functional/hash.hpp>
 using i64= std::int64_t;
 using cell= std::pair<i64,i64>;
 i64 gcd( i64 x, i64 y ) { return not y ? x:gcd(y,x%y); }
@@ -20,67 +21,63 @@ int sign_of( i64 x ) {
     return x < 0 ? -1:1;
 }
 
+struct frac {
+    std::pair<i64,i64> val;
+    std::pair<double,double> value;
+    double w;
+    explicit frac() {}
+    explicit frac( i64 x, i64 y ) {
+        val= {x,y};
+        value= x==0?std::make_pair(0.00,0.00):std::make_pair(x+0.00,y+0.00);
+        if ( x != 0 )
+            w= value.first/value.second;
+    }
+};
+
+bool operator < ( const frac &a, const frac &b ) {
+    if ( a.val.first == 0 ) {
+        return b.val.first != 0;
+    }
+    if ( b.val.first == 0 ) {
+        return false ;
+    }
+    //return a.value.first/a.value.second < b.value.first/b.value.second;
+    return a.w < b.w;
+}
+
+bool operator == ( const frac &a, const frac &b ) {
+    return a.val == b.val;
+}
+
 struct angle {
-    std::pair<i64,i64> cosine;
-    std::pair<i64,i64> sine;
+    frac cosine;
+    frac sine;
     int sc;
     int ss;
+    mutable int q= -1;
     int quadrant() const {
+        if ( q != -1 )
+            return q;
         if ( sc >= 0 and ss >= 0 )
-            return 1;
+            return q= 1;
         if ( sc <= 0 and ss >= 0 )
-            return 2;
+            return q= 2;
         if ( sc >= 0 and ss <= 0 )
-            return 4;
-        return 3;
+            return q= 4;
+        return q= 3;
     }
 };
 
-struct comparator {
-    bool operator() ( const std::pair<i64,i64> &a, const std::pair<i64,i64> &b ) const {
-        assert( a.second >= 1 and b.second >= 1 );
-        if ( a.first == 0 ) {
-            return b.first != 0;
-        }
-        if ( b.first == 0 ) {
-            return false ;
-        }
-        return a.first < a.second*(b.first/(b.second+0.00));
-    }
-};
-
-comparator C{};
-
-std::set<std::pair<int,int>> rel= {{3,4},{4,1},{1,2},{3,2},{3,1},{4,2}};
+std::set<std::pair<int,int>> rel= {{3,4},{4,1},{1,2}};
 bool is_less[5][5];
 
-bool compareFirstQuadrant( const angle &a, const angle &b ) {
-    assert( a.quadrant() == 1 and b.quadrant() == 1 );
-    return C(b.cosine,a.cosine);
-}
-
-bool compareSecondQuadrant( const angle &a, const angle &b ) {
-    assert( a.quadrant() == 2 and b.quadrant() == 2 );
-    return C(b.sine,a.sine);
-}
-
-bool compareThirdQuadrant( const angle &a, const angle &b ) {
-    assert( a.quadrant() == 3 and b.quadrant() == 3 );
-    return C(b.cosine,a.cosine);
-}
-
-bool compareFourthQuadrant( const angle &a, const angle &b ) {
-    assert( a.quadrant() == 4 and b.quadrant() == 4 );
-    return C(a.cosine,b.cosine);
-}
-
-bool operator < (const angle &a, const angle &b ) {
+bool operator < ( const angle &a, const angle &b ) {
     if ( a.quadrant() == b.quadrant() ) {
         switch ( a.quadrant() ) {
-            case 1: return compareFirstQuadrant(a,b);
-            case 2: return compareSecondQuadrant(a,b);
-            case 3: return compareThirdQuadrant(a,b);
-            case 4: return compareFourthQuadrant(a,b);
+            case 1: return b.cosine < a.cosine;
+            case 2: return b.sine < a.sine;
+            case 3: return b.cosine < a.cosine;
+            case 4: return a.cosine < b.cosine;
             default: assert( false );
         }
     } else {
@@ -99,15 +96,15 @@ angle get_ang( const cell &a, const cell &b ) {
     i64 len= co + si;
     i64 cc= gcd(co,len), ss= gcd(si,len);
     angle ang{
-        .cosine= {co/cc,len/cc},
-        .sine  = {si/ss,len/ss},
+        .cosine= frac{co/cc,len/cc},
+        .sine  = frac{si/ss,len/ss},
         .sc= sign_of(u),
         .ss= sign_of(v)
     };
     return ang;
 }
 
-angle rotate90( angle &a ) {
+angle rotate90( const angle &a ) {
     angle b{
         .cosine= a.sine,
         .sine= a.cosine,
@@ -116,6 +113,29 @@ angle rotate90( angle &a ) {
     };
     return b;
 }
+
+template<>
+struct std::hash<angle> {
+    std::hash<i64> h{};
+    std::size_t operator() ( const angle &a ) const {
+        using boost::hash_value;
+        using boost::hash_combine;
+
+        // Start with a hash value of 0    .
+        std::size_t seed = 0;
+
+        // Modify 'seed' by XORing and bit-shifting in
+        // one member of 'Key' after the other:
+        hash_combine(seed,hash_value(a.sine.val.first));
+        hash_combine(seed,hash_value(a.sine.val.second));
+        hash_combine(seed,hash_value(a.cosine.val.first));
+        hash_combine(seed,hash_value(a.cosine.val.second));
+
+        // Return the result.
+        return seed;
+        //return h(a.sine.val.first)^h(a.sine.val.second)^h(a.cosine.val.first)^h(a.cosine.val.second);
+    }
+};
 
 i64 f( const int idx ) {
     i64 ans= 0;
@@ -133,20 +153,45 @@ i64 f( const int idx ) {
     return ans;
 }
 
+i64 ff( const int idx ) {
+    i64 ans= 0;
+    int i;
+    std::unordered_map<angle,i64> cnt[3][3];
+    for ( i= 0; i < n; ++i )
+        if ( i != idx ) {
+            auto z= get_ang(pts[idx],pts[i]);
+            ++cnt[z.sc+1][z.ss+1][z];
+        }
+    for ( int sc= -1; sc <= 1; ++sc )
+        for ( int ss= -1; ss <= 1; ++ss ) {
+        for (const auto &[key, val]: cnt[sc+1][ss+1]) {
+            auto T = rotate90(key);
+            i64 kappa = cnt[T.sc+1][T.ss+1].count(T) ? cnt[T.sc+1][T.ss+1][T] : 0;
+            ans += val * kappa;
+        }
+    }
+    return ans;
+}
+
 int main() {
     std::ios_base::sync_with_stdio(false), std::cin.tie(nullptr);
 #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
 #endif
     memset(is_less,0,sizeof is_less);
-    for ( auto it: rel )
+    for ( auto &it: rel )
         is_less[it.first][it.second]= true ;
+    for ( int k= 1; k <= 4; ++k )
+        for ( int i= 1; i <= 4; ++i )
+            for ( int j= 1; j <= 4; ++j )
+                if ( is_less[i][k] and is_less[k][j] )
+                    is_less[i][j]= true ;
     for ( ;(is >> n) and n; ) {
         pts.resize(n);
         for ( auto &v: pts )
             is >> v.first >> v.second;
         i64 ans= 0;
-        for ( int i= 0; i < n; ans+= f(i++) ) ;
+        for ( int i= 0; i < n; ans+= ff(i++) ) ;
         os << ans << '\n';
     }
     return 0;
