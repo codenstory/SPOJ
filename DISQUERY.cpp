53299f25bbe3a80cbd8bf7c1b54b8efdb58c0516
kazi.serikzhan@gmail.com <Dalgrad2020>
2020-06-29 18:40:51 -0300

    DISQUERY WIP

--- DISQUERY.cpp
+++ DISQUERY.cpp
@@ -10,31 +10,34 @@ class tree {
     enum { MN= 0, MX= 1 };
 
     using t_node= std::int32_t;
-    t_node to[MAXE],mapping[N],V,inv[N];
-    int n,next[MAXE],last[N],E,w[N],distance[N],
-        p[N],longest[N],best[N],
-        which_ladder[N],pos[N],d[N];
-    std::vector<std::vector<int>> ladders, dp[2];
-    std::vector<std::vector<t_node>> anc;
-    bool seen[N];
+    using t_weight= std::int32_t;
 
-    void add_arc( t_node x, t_node y, int weight ) {
+    const t_weight oo= (1<<29);
+
+    bool seen[N];
+    t_node to[MAXE],p[N];
+    t_weight w[N],longest[N],d[N];
+    int n,next[MAXE],last[N],E,best[N],
+        which_ladder[N],pos[N];
+    std::vector<std::vector<t_node>> ladders, anc;
+    std::vector<std::vector<t_weight>> dp[2];
+    std::vector<std::vector<std::pair<t_node,t_weight>>> adj;
+
+    void add_arc( t_node x, t_node y, t_weight weight ) {
         int i= E++, j= E++;
         to[i]= y, next[i]= last[x], last[x]= i;
         to[j]= x, next[j]= last[y], last[y]= j;
         w[i]= w[j]= weight;
     }
 
-    std::vector<std::vector<std::pair<t_node,int>>> adj;
-
     void dfs( t_node x ) {
         assert( not seen[x] ) ;
-        seen[x]= true, inv[mapping[x]= V++]= x+1;
+        seen[x]= true;
         for ( auto i= last[x]; i != -1; i= next[i] ) {
             auto y= to[i];
             if ( not seen[y] ) {
-                dfs(y), p[mapping[y]]= mapping[x];
-                adj[mapping[x]].push_back({mapping[y],w[i]});
+                dfs(y), p[y]= x;
+                adj[x].push_back({y,w[i]});
             }
         }
     }
@@ -45,11 +48,11 @@ class tree {
         int k;
         for ( auto [y,wgt]: adj[x] ) {
             assert( not seen[y] );
-            d[y]= d[x]+1, distance[y]= distance[x]+wgt;
-            anc[y].resize(log(d[y])/log(2)+2);
+            d[y]= d[x]+1;
+            size_t sz= 33-__builtin_clz(d[y]);
+            anc[y].resize(sz,-1), dp[MN][y].resize(sz,+oo), dp[MX][y].resize(sz,-oo);
             for ( anc[y][0]= x, k= 1; (1<<k) <= d[y] and anc[y][k-1] != -1; anc[y][k]= anc[anc[y][k-1]][k-1], ++k ) ;
-            dp[MN][y][0]= dp[MX][y][0]= wgt;
-            for ( k= 1; (1<<k) <= d[y] and anc[y][k-1] != -1; ++k ) {
+            for ( dp[MN][y][0]= dp[MX][y][0]= wgt, k= 1; (1<<k) <= d[y] and anc[y][k-1] != -1; ++k ) {
                 dp[MN][y][k]= std::min(dp[MN][y][k-1],dp[MN][anc[y][k-1]][k-1]);
                 dp[MX][y][k]= std::max(dp[MX][y][k-1],dp[MX][anc[y][k-1]][k-1]);
             }
@@ -58,13 +61,13 @@ class tree {
     }
 
     int calc_ladder_length( t_node x ) {
-        if ( seen[x] )
-            return longest[x];
+        assert( not seen[x] );
         seen[x]= true, longest[x]= 1, best[x]= -1;
         for ( int i= 0; i < adj[x].size(); ++i ) {
             auto y= adj[x][i].first;
             assert( not seen[y] );
-            if ( (longest[x]= std::max(longest[x],1+calc_ladder_length(y))) == 1+calc_ladder_length(y) )
+            auto len= 1+calc_ladder_length(y);
+            if ( (longest[x]= std::max(longest[x],len)) == len )
                 best[x]= i;
         }
         return longest[x];
@@ -73,17 +76,24 @@ class tree {
     void preprocess() {
         int x,y,k;
         for ( x= 0; x < n; seen[x++]= false ) ;
-        calc_ladder_length(x);
+        calc_ladder_length(0);
         std::queue<int> q{};
-        for ( ladders.clear(), q.push(0); not q.empty(); q.pop() ) {
+        for ( x= 0; x < n; seen[x++]= false ) ;
+        std::unordered_set<t_node> in_chain{};
+        for ( ladders.clear(), seen[0]= true, q.push(0); not q.empty(); q.pop() ) {
             auto X= q.front();
             x= q.front();
             std::vector<int> chain{};
             chain.push_back(x);
             for ( int i=best[x]; i!=-1; x=adj[x][i].first, chain.push_back(x), i=best[x] ) ;
+            for ( auto z: chain ) {
+                assert( not in_chain.count(z) );
+                seen[z] = true, in_chain.insert(z);
+            }
             for ( auto z: chain )
-                for ( auto [nd,_]: adj[z] )
-                    q.push(nd);
+                for ( auto [t,_]: adj[z] )
+                    if ( not seen[t] )
+                        seen[t]= true, q.push(t);
             for ( auto z: chain )
                 which_ladder[z]= ladders.size();
             std::reverse(chain.begin(),chain.end());
@@ -98,16 +108,20 @@ class tree {
 
     int min_query( t_node px, t_node x ) {
         if ( px == x )
-            return std::numeric_limits<int>::max();
+            return +oo;
+        assert( d[px] < d[x] );
         auto k= 31-__builtin_clz(d[x]-d[px]);
-        return std::max(dp[MX][x][k],dp[MX][level_ancestor(inv[x],d[x]-d[px]-(1<<k))][k]);
+        assert( (1ll<<k) <= d[x]-d[px] and (1ll<<k) >= (d[x]-d[px])/2 );
+        return std::min(dp[MN][x][k],dp[MN][level_ancestor(x,d[x]-d[px]-(1<<k))][k]);
     }
 
     int max_query( t_node px, t_node x ) {
         if ( px == x )
-            return std::numeric_limits<int>::max();
+            return -oo;
+        assert( d[px] < d[x] );
         auto k= 31-__builtin_clz(d[x]-d[px]);
-        return std::max(dp[MN][x][k],dp[MN][level_ancestor(inv[x],d[x]-d[px]-(1<<k))][k]);
+        assert( (1ll<<k) <= d[x]-d[px] and (1ll<<k) >= (d[x]-d[px])/2 );
+        return std::max(dp[MX][x][k],dp[MX][level_ancestor(x,d[x]-d[px]-(1<<k))][k]);
     }
 
 public:
@@ -117,30 +131,28 @@ public:
         int x,y,cost;
         for ( d[0]= 0, x= 0; x < n; seen[x]= false, last[x++]= -1 ) ;
         for ( int k= 0; k < n-1; is >> x >> y >> cost, add_arc(x-1,y-1,cost), ++k ) ;
-        adj.resize(n,std::vector<std::pair<t_node,int>>{});
-        V= 0, dfs(0);
-        assert( V == n );
-        anc.resize(n), dp[MN].resize(n), dp[MX].resize(n);
+        adj.resize(n,std::vector<std::pair<t_node,t_weight>>{});
+        dfs(0);
+        anc.resize(n,std::vector<t_node>{});
+        dp[MN].resize(n,std::vector<t_weight>{}), dp[MX].resize(n,std::vector<t_weight>{});
         for ( preprocess(), x= 0; x < n; seen[x++]= false ) ;
         calc_anc(0);
     }
 
     t_node level_ancestor( t_node x, int k ) {
-        x= mapping[x-1];
         if ( k == 0 )
-            return inv[x];
+            return x;
         if ( k >= d[x] )
             return 0;
-        auto i= 31-__builtin_clz(k);
-        auto y= anc[x][i];
-        assert( pos[y] >= (k-(1<<i)) );
-        return inv[ladders[which_ladder[y]][pos[y]-(k-(1<<i))]];
+        if ( pos[x] >= k )
+            return ladders[which_ladder[x]][pos[x]-k];
+        auto i= 31-__builtin_clz(d[x]-k);
+        return level_ancestor(anc[x][i],k-(1<<i));
     }
 
     t_node lca( t_node x, t_node y ) {
         if ( x == y )
             return x;
-        x= mapping[x-1], y= mapping[y-1];
         int good= std::max(d[x],d[y]), bad= 0;
         for (;bad+1 != good;) {
             auto mid= (good+bad)/2;
@@ -151,13 +163,8 @@ public:
         return level_ancestor(x,good);
     }
 
-    int cost( t_node px, t_node x ) {
-        return distance[mapping[x-1]]-distance[mapping[px-1]];
-    }
-
     std::pair<int,int> query( t_node x, t_node y ) {
         auto z= lca(x,y);
-        x= mapping[x-1], y= mapping[y-1], z= mapping[z-1];
         return {
             std::min(min_query(z,x),min_query(z,y)),
             std::max(max_query(z,x),max_query(z,y))
@@ -177,16 +184,16 @@ int main() {
     std::istream &is = std::cin;
     std::ostream &os = std::cout;
 
-    auto T= new tree(is);
-    int x,y,qr,a,b;
-    for ( is >> qr; qr--; ) {
-        is >> x >> y;
-        std::tie(a,b)= T->query(x,y);
-        os << a << ' ' << b << '\n';
+    for ( int ts= 0; ts < 2; ++ts ) {
+        auto T = new tree(is);
+        int x, y, qr, a, b;
+        for (is >> qr; qr--;) {
+            is >> x >> y;
+            std::tie(a, b) = T->query(x - 1, y - 1);
+            os << a << ' ' << b << '\n';
+        }
+        delete T;
     }
-    delete T;
-
     return 0;
-
 }
     
